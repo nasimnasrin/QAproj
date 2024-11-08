66e86e9bfb0a1a1b4a80a27659eeb3d76c659952
mak <narsisus072@gmail.com>
2024-11-07 20:57:17 -0500

    Maksuda made some changes

--- src/test/java/print/printname/printname.java
+++ src/test/java/print/printname/printname.java
@@ -5,7 +5,7 @@ public class printname {
 	public static void main(String[] args) {
 		System.out.println("my name is nasim");
 		// TODO Auto-generated method stub
-
+		System.out.println("My name is Maksuda");
 	}
 
 }
